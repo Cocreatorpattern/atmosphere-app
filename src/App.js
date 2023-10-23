e2131a53ffa14977b236c7b137a02caf2bc67b16
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-10-23 22:15:24 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <button className="btn btn-primary"> Here</button>
+      <footer>
+        <a
+          href="https://iridescent-faun-662cb2.netlify.app/"
+          target="blank"
+          rel="noreferrer"
+        >
+          Nneka Umerah{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/Cocreatorpattern/atmosphere-app"
+          target="blank"
+          rel="noreferre"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
