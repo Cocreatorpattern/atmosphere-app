6b7cdcde4898f50676b9e1022a9ae26ec0a6d479
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-10-25 21:45:08 +0100

    added axios ,api key and made axios call

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React,{useState}  from "react";
+
+import axios from "axios";
 import "./Weather.css";
 
 
 
 
-export default function Weather () {
-return (
+export default function Weather (props) {
+
+const [weatherData,setWeatherData]=useState({ ready: false });
+function handleResponse(response) {
+console.log(response.data);
+setWeatherData({
+    ready: true,
+    coordinates: response.data.coord,
+    temperature: response.data.main.temp,
+    humidity: response.data.main.humidity,
+    description: response.data.weather[0].description,
+    iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    wind: response.data.wind.speed,
+    city: response.data.name,
+});
+
+
+    
+}
+
+if (weatherData.ready){
+ return (
 <div className ="Weather">
 <form>
     <div className="row">
@@ -22,28 +44,36 @@ return (
 </div>
 </div>
 </form>
-<h1>Manchester</h1>
+<h1>{weatherData.city}</h1>
 <ul>
  <li>Tuesday 18:00 </li>
- <li>Mostly Cloudy  </li>
+ <li>{weatherData.description} </li>
 </ul>
 <div className="row mt-3">
 <div className="col-6">
-<div className="clearfix">
-<img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" className="float-left"/>
-<span className="temperature">6</span>
+<div className="clearfix d-flex align-items-center">
+<img src={weatherData.iconUrl} alt={weatherData.description} className="float-left"/>
+<span className="temperature">{Math.round(weatherData.temperature)}</span>
 <span className="unit">°C</span>
 </div>
 </div>
 </div>
 <div className="col-6">
  <ul>
-  <li>Precipitation:22%</li>
-  <li>Humdity:91%</li>
-  <li>Wind:2mph</li>
+  
+  <li>Humdity:{weatherData.humidity}</li>
+  <li>Wind:{weatherData.wind}</li>
  </ul>
 </div>
 </div>
 
 );
+}else{
+const apiKey = "c2664ee5d87203ac5037da5b7330b87c";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
+
+return "Loading....";
+
+}
 }
