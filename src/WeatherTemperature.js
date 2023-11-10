f4b9580f83aed66eaf10afcc8d60116c28192419
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-11-10 16:04:41 +0000

    removed fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,53 +1,10 @@
-import React, {useState} from "react";
-
-
+import React from "react";
 
 export default function WeatherTemperature(props) {
-const [unit, setUnit] = useState ("metric");
-
-function showFahrenheit (event){
-event.preventDefault();
-setUnit("imperial");
-}
-  
-
-function showCelsius(event) {
-event.preventDefault();
-setUnit("metric");
-    
-}
-
-function convertToFahrenheit () {
-return (props.celsius * 9) / 5 + 32;
-    
-}
-  
- 
-if (unit ==="metric"){
-return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">
-        °C|{""}
-      <a href="/" onClick={showFahrenheit}>
-        °F
-      </a>
-      </span>
-    </div>
-  );
-}else{
   return (
     <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(convertToFahrenheit())}</span>
-      <span className="unit">
-        
-      <a href="/"  onClick={showCelsius}>
-        °C|{""}
-      </a>
-      |°F
-      </span>
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
     </div>
   );
-  
-}
 }
