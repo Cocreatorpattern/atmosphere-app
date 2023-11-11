fef4850e01ca1b6e384701c794329108d63c1a60
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-11-11 01:32:12 +0000

    added background image

--- src/Weather.js
+++ src/Weather.js
@@ -54,7 +54,7 @@ export default function Weather(props) {
               />
             </div>
             <div className="col-3">
-              <input type="submit" value="search" className="btn btn-primary" />
+              <input type="submit" value="search" className="btn btn-warning" />
             </div>
           </div>
         </form>
